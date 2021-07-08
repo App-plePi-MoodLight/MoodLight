3478dea03957c9d3edb88b38e98a485ad6cc272b
inseong04 <ois0531@naver.com>
2021-07-08 22:56:46 +0900

    initial commit

--- app/src/main/java/com/example/moodlight/screen/MainActivity.kt
+++ app/src/main/java/com/example/moodlight/screen/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.moodlight.screen
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.moodlight.R
 import com.example.moodlight.screen.main1.MainFragment1
 import com.example.moodlight.screen.main2.MainFragment2
 import com.example.moodlight.screen.main3.MainFragment3
+import com.example.moodlight.util.NetworkStatus
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 @Suppress("DEPRECATION")
@@ -16,11 +17,17 @@ class MainActivity : AppCompatActivity() {
     private val mainFragment1 by lazy {MainFragment1()}
     private val mainFragment2 by lazy {MainFragment2()}
     private val mainFragment3 by lazy {MainFragment3()}
+    private val networkStatus : Int by lazy {NetworkStatus.getConnectivityStatus(applicationContext)}
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         changeFragment(mainFragment1)
+
+        if (networkStatus == NetworkStatus.TYPE_NOT_CONNECTED) {
+            Toast.makeText(baseContext, "무드등을 이용하시려면 Wifi연결이 필요합니다.", Toast.LENGTH_SHORT).show()
+        }
 
         findViewById<BottomNavigationView>(R.id.bottomNavigation).setOnItemSelectedListener { item ->
             when (item.itemId) {

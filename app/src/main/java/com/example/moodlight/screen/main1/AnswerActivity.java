ffc0d043fa5d8791aa6fffe745cf99d4cf9ea287
sh596 <sh596@naver.com>
2021-07-19 13:45:42 +0900

    게시물 관련 xml 작성

---
+++ app/src/main/java/com/example/moodlight/screen/main1/AnswerActivity.java
@@ -0,0 +1,16 @@
+package com.example.moodlight.screen.main1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.moodlight.R;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
+}
